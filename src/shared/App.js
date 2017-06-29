4268029f1d0827ac61474c8b08e8531ce7bd48fa
Jeckson <cjswp122@gmail.com>
2017-06-29 15:46:14 +0900

    라우트 code split

--- src/shared/App.js
+++ src/shared/App.js
@@ -4,10 +4,12 @@ import { Home, About, Posts } from 'pages';
 import Menu from 'components/Menu';
 
 class App extends Component {
+
     render() {
         return (
             <div>
                 <Menu/>
+                
                 <Route exact path='/' component={Home}/>
                 <Switch>
                     <Route path='/about/:name' component={About}/>
